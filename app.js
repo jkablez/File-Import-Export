af24c8bb20ae7a952b41f02a3aaf601ca993b677
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-27 22:33:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fileDropdownLinks(
+fileDropdownLinks([
   {
     label: "Toggle Handle",
     button: true,
@@ -10,4 +10,4 @@ fileDropdownLinks(
     href: "/dashboard/plugin/jkablez/mediah-plugin/config",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 -mt-[1px]"><path d="M20 7h-9"/><path d="M14 17H5"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></svg>`
   }
-)
+])
