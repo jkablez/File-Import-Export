25c3b321c8034f6849c60edef60589c54d8e26c3
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-27 22:13:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
-if (location.pathname === "/dashboard/files") {
-  showAlert("Save file to JSON, intialized", "success");
-
-  function saveFileButtons() {
-    var table = document.querySelector("tbody");
-    var tableRows = table.querySelectorAll("tr");
-    
-    tableRows.forEach(async (row) => {
-      const fileId = row.querySelector('[data-id]').getAttribute("data-id");
-      const dropdown = row.querySelector(".dropdown");
-      const dropdownGroup = dropdown.querySelector("div");
-  
-      const button = document.createElement("button");
-      button.textContent = "Save File JSON";
-      button.classList = "p-1.5 px-2 hover:bg-foreground/5 transition-all rounded-md flex items-center text-sm capitalize";
-  
-      button.addEventListener("click", async () => {
-        const response = await fetch(`/api/file/${fileId}`);
-        const fileData = await response.json();
-        console.log(fileData);
-      });
-      
-      dropdownGroup.appendChild(button);
-    })
+fileDropdownLinks(
+  {
+    label: "Toggle Handle",
+    button: true,
+    id: "toggle-handle",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 -mt-[1px]"><path d="M18 11V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2"/><path d="M14 10V4a2 2 0 0 0-2-2a2 2 0 0 0-2 2v2"/><path d="M10 10.5V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2v8"/><path d="M18 8a2 2 0 1 1 4 0v6a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L7 15"/></svg>`
+  },
+  {
+    label: "Config",
+    href: "/dashboard/plugin/jkablez/mediah-plugin/config",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 -mt-[1px]"><path d="M20 7h-9"/><path d="M14 17H5"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></svg>`
   }
-
-  document.addEventListener("fileSort", () => {
-    saveFileButtons();
-  })
-
-  saveFileButtons();
-}
+)
