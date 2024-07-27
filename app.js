6af58b3d938db774a0601a05c584dee4c79946f3
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-27 23:28:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,32 +6,38 @@ fileDropdownLinks([
   }
 ])
 
-document.querySelector(".content").addEventListener('filesLinksInit', (event) => {
-  var table = document.querySelector("tbody");
-  var tableRows = table.querySelectorAll("tr");
+document.querySelector(".content").addEventListener('filesLinksInit', () => {
+  try {
+    var table = document.querySelector("tbody");
+    var tableRows = table.querySelectorAll("tr");
 
-  tableRows.forEach((row) => {
-    const exportButton = row.querySelector('[data-id="export-as-json"]');
-    exportButton.addEventListener("click", async () => {
-      const fileId = row.getAttribute("data-id");
-      const response = await fetch(`/api/file/${fileId}`);
-      const fileData = await response.json();
+    tableRows.forEach((row) => {
+      try {
+        const exportButton = row.querySelector('[data-id="export-as-json"]');
+        exportButton.addEventListener("click", async () => {
+          const fileId = row.getAttribute("data-id");
+          const response = await fetch(`/api/file/${fileId}`);
+          const fileData = await response.json();
 
-      const jsonString = JSON.stringify(fileData, null, 2);
-      const blob = new Blob([jsonString], { type: "application/json" });
+          const jsonString = JSON.stringify(fileData, null, 4);
+          const blob = new Blob([jsonString], { type: "application/json" });
 
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = fileData.Filename.replace(/\.[^/.]+$/, ".json");
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = fileData.Filename.replace(/\.[^/.]+$/, ".json");
 
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
 
-      URL.revokeObjectURL(url);
+          URL.revokeObjectURL(url);
 
-      showAlert(`Downloaded '${fileData.Filename}' as a raw JSON file`, "success");
+          showAlert(`Downloaded '${fileData.Filename}' as a raw JSON file`, "success");
+        })
+      }
+      catch { };
     })
-  })
+  }
+  catch { };
 });
