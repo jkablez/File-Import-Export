d383365dd9d78e222fbe88f6f5952d34c5d0b417
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-28 10:32:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-this.addSideBarGroup("File Import & Export", [{
+this.sideBarEdit("File Import & Export", [{
   label: "Import JSON",
   button: true,
-  id: "import-file-json", // HTML Id
+  id: "import-file-json",
   icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 -mt-[1px]"><path d="M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M4 12a1 1 0 0 0-1 1v1a1 1 0 0 1-1 1 1 1 0 0 1 1 1v1a1 1 0 0 0 1 1"/><path d="M8 18a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1 1 1 0 0 1-1-1v-1a1 1 0 0 0-1-1"/></svg>`
 }]);
 
