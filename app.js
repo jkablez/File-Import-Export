6e1272a9438a14319976ed88e70ffad5fa0bc202
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-28 10:28:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+this.addSideBarGroup("File Import & Export", [{
+  label: "Import JSON",
+  button: true,
+  id: "import-file-json", // HTML Id
+  icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 -mt-[1px]"><path d="M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M4 12a1 1 0 0 0-1 1v1a1 1 0 0 1-1 1 1 1 0 0 1 1 1v1a1 1 0 0 0 1 1"/><path d="M8 18a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1 1 1 0 0 1-1-1v-1a1 1 0 0 0-1-1"/></svg>`
+}]);
+
 fileDropdownLinks([
   {
     label: "Export as JSON",
@@ -13,30 +20,30 @@ document.querySelector(".content").addEventListener('filesLinksInit', () => {
 
     tableRows.forEach((row) => {
       try {
-        if (row) {   
+        if (row) {
           const exportButton = row.querySelector('[data-id="export-as-json"]');
           exportButton.addEventListener("click", async () => {
             const fileId = row.getAttribute("data-id") || false;
             if (fileId) {
               const response = await fetch(`/api/file/${fileId}`);
               const fileData = await response.json();
-    
+
               const jsonString = JSON.stringify(fileData, null, 4);
               const blob = new Blob([jsonString], { type: "application/json" });
-    
+
               const url = URL.createObjectURL(blob);
               const a = document.createElement("a");
               a.href = url;
               a.download = fileData.Filename.replace(/\.[^/.]+$/, ".json");
-    
+
               document.body.appendChild(a);
               a.click();
               document.body.removeChild(a);
-    
+
               URL.revokeObjectURL(url);
-    
+
               showAlert(`Downloaded '${fileData.Filename}' as a raw JSON file`, "success");
-  
+
               return;
             }
           })
